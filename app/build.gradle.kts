721199f1b8335928062bfec16057e2ff6ca762d3
gracen-greenburg <gracenkg@email.sc.edu>
2026-03-29 13:35:18 -0400

    Import error fixes?

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -60,6 +61,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
     
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/example/improvable/data/GamesInfo.kt
+++ app/src/main/java/com/example/improvable/data/GamesInfo.kt
@@ -1,7 +1,12 @@
 package com.example.improvable.data
 // 3/29
+import kotlinx.serialization.InternalSerializationApi
 import java.util.Date // I want it to track when a game was last played eventully
 
+import kotlinx.serialization.Serializable
+
+@OptIn(InternalSerializationApi::class) // nightmare solution, but it works
+@Serializable
 data class GamesInfo (
     val id: String,
     val title: String,
