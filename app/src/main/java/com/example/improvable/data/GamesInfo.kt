a92d064bfc0fface3c2530e4fccfd46c042656d3
gracen-greenburg <gracenkg@email.sc.edu>
2026-03-29 11:10:33 -0400

    Adding Game Info JSON, data//assets folder

---
+++ app/src/main/java/com/example/improvable/data/GamesInfo.kt
@@ -0,0 +1,13 @@
+package com.example.improvable.data
+// 3/29
+import java.util.Date // I want it to track when a game was last played eventully
+
+data class GamesInfo (
+    val id: String,
+    val title: String,
+    val minPlayers: Int,
+    val maxPlayers: Int?, // some games don't have cap
+    val description: String,
+    val lastPlayed: Long? = null, // timestamp
+    val tags: List<String> = emptyList() // SO WE CAN USE A SEARCH FUNCTION
+)
