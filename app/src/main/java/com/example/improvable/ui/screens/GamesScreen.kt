291e7d49d9df6dba585818e6556721a0b36dce5e
Jsaur <6awesomejoe@gmail.com>
2026-03-30 17:16:34 -0400

    make games information collapsable

--- app/src/main/java/com/example/improvable/ui/screens/GamesScreen.kt
+++ app/src/main/java/com/example/improvable/ui/screens/GamesScreen.kt
@@ -1,5 +1,13 @@
 package com.example.improvable.ui.screens
 
+import android.opengl.Visibility
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.VisibilityThreshold
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,9 +22,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.improvable.data.GamesInfo
@@ -63,22 +75,43 @@ fun GamesScreen( // adding the viewmodel so we can change screen
     }
 }
 
+// used https://composeexamples.com/components/application-ui/components/accordions for guidance
 @Composable // DISPLAY GAME INFORMATION
 fun GameItem(game: GamesInfo) { // pulling from json file
+    var expanded = remember { mutableStateOf(false) }
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp)
+            .clickable{expanded.value = expanded.value.not()}
     ) {
         Text(text = game.title, style = MaterialTheme.typography.titleLarge)
-        Text(text = "Players: ${game.minPlayers}${if (game.maxPlayers != null) " - ${game.maxPlayers}" else "+"}")
-        Text(text = game.description, style = MaterialTheme.typography.bodyMedium)
-        if (game.tags.isNotEmpty()) {
-            Text(
-                text = "Tags: ${game.tags.joinToString(", ")}",
-                style = MaterialTheme.typography.labelSmall,
-                color = MaterialTheme.colorScheme.primary
+    }
+    AnimatedVisibility(
+        visible = expanded.value,
+        enter = expandVertically(
+            spring(
+                stiffness = Spring.StiffnessMediumLow,
+                visibilityThreshold = IntSize.VisibilityThreshold
             )
+        ),
+        exit = shrinkVertically()
+
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        ) {
+            Text(text = "Players: ${game.minPlayers}${if (game.maxPlayers != null) " - ${game.maxPlayers}" else "+"}")
+            Text(text = game.description, style = MaterialTheme.typography.bodyMedium)
+            if (game.tags.isNotEmpty()) {
+                Text(
+                    text = "Tags: ${game.tags.joinToString(", ")}",
+                    style = MaterialTheme.typography.labelSmall,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
         }
     }
 }
