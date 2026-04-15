2e36e76abe24035e04ca48945d4089df91c131b5
Jsaur <6awesomejoe@gmail.com>
2026-04-15 17:41:16 -0400

    fix #29 - save sessions to sharedprefs but now it is not linked to the actual json file

--- app/src/main/java/com/example/improvable/ui/screens/SuggestionsViewModel.kt
+++ app/src/main/java/com/example/improvable/ui/screens/SuggestionsViewModel.kt
@@ -18,6 +18,7 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.io.File
 import java.io.OutputStream
+import androidx.core.content.edit
 
 class SuggestionsViewModel(private val context: Context) : ViewModel() {
     private var _questions = MutableStateFlow<List<String>>(listOf<String>())
@@ -41,7 +42,8 @@ class SuggestionsViewModel(private val context: Context) : ViewModel() {
     private fun loadSessions() {
         viewModelScope.launch {
             try {
-                val jsonString = context.assets.open("suggestionLists.json").bufferedReader().use { it.readText() }
+//                val jsonString = context.assets.open("suggestionLists.json").bufferedReader().use { it.readText() }
+                val jsonString = loadSessionsJsonStringFromPrefs(context)
                 val allSuggestions = Json.decodeFromString<ArrayList<SuggestionsList>>(jsonString)
                 var i = 0;
                 while (i < allSuggestions.size) {
@@ -69,19 +71,24 @@ class SuggestionsViewModel(private val context: Context) : ViewModel() {
     fun addSuggestion(suggestion: String) {
         if (!_generalSuggestions.value.contains(suggestion)) {
             _generalSuggestions.value.add(suggestion)
-            saveSuggestions()
+            saveSessionsToPrefs(context)
         }
     }
 
-    fun saveSuggestions() {
+    // --- These should probably be in their own file and use constants for the string values --- \\
+    private fun prefs(context: Context) =
+        context.getSharedPreferences("improvableSharedPrefs", Context.MODE_PRIVATE)
+
+    fun saveSessionsToPrefs(context: Context) {
         val allSuggestions = ArrayList<SuggestionsList>()
         allSuggestions.add(SuggestionsList("questions", _questions.value))
         allSuggestions.add(SuggestionsList("general suggestions", _generalSuggestions.value))
         val jsonString = Json.encodeToString(allSuggestions)
-//        Log.d("D", "FIRST: " + jsonString)
-        context.openFileOutput("suggestionLists.json", Context.MODE_PRIVATE).use {
-            it.write(jsonString.toByteArray())
-        }
-//        Log.d("D", "SECOND: " + context.assets.open("suggestionLists.json").bufferedReader().use { it.readText() })
+        prefs(context).edit { putString("suggestionLists.json", jsonString) }
+    }
+
+    fun loadSessionsJsonStringFromPrefs(context: Context) : String {
+        val p = context.getSharedPreferences("improvableSharedPrefs", Context.MODE_PRIVATE)
+        return p.getString("suggestionLists.json", "")!!
     }
 }
